10dabfe08d55acb44016f69bbbf8414ef0ff5bc7
leiyiming7002 <1083916092@qq.com>
2026-03-14 14:52:49 +0800

    feat: add HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// 定义一个公开类，类名为 HelloWorld
+public class HelloWorld {
+
+    // 程序入口方法，JVM 会从这里开始执行
+    public static void main(String[] args) {
+        // 在控制台输出一行文本：Hello, World!
+        System.out.println("Hello, World!");
+    }
+}
